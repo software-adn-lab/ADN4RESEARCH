5d0d16a85c1712caceb105e089cc2f95da442124
jluSalazar <jonathan282salazar@gmail.com>
2025-11-26 01:15:13 -0500

    deleted: refactored architecture

--- apps/extraction/domain/value_objects/extraction_mode.py
+++
@@ -1,9 +0,0 @@
-from enum import Enum
-
-class ExtractionMode(str, Enum):
-    """Modo de extracción"""
-    SINGLE = 'Single'  # Un investigador por estudio
-    DOUBLE = 'Double'  # Dos investigadores (extracción por pares)
-
-    def __str__(self):
-        return self.value
