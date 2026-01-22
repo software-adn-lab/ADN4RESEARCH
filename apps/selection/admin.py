aee5f07cbe9ae7a1f419fc8062d5eb75afe7cf17
Alejandro Jiménez <alejandro.jimenez@sideralsoft.com>
2026-01-22 07:33:01 -0500

    feat: adds models to admin.py

--- apps/selection/admin.py
+++ apps/selection/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from apps.selection.features.distribution.models import SelectionPhase
+from apps.selection.features.screening.models import PaperAssignment, PaperReview
+from apps.selection.features.discussion.models import ConflictResolution
+
+admin.site.register(SelectionPhase)
+admin.site.register(PaperAssignment)
+admin.site.register(PaperReview)
+admin.site.register(ConflictResolution)
